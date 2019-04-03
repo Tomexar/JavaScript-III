159de0eef7374c5ad2fed6113b58739b3c0f9134
Tomexar <tommyexar@gmail.com>
2019-04-03 14:47:44 -0600

    working on this

--- assignments/this.js
+++ assignments/this.js
@@ -1,9 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
+* 1. Without pointing to anything "this" with be bound to the window object.
+* 2. Implicit - when a function is called using dot notation the object before the . is what this points to 
+* 3. New- when a constructior function is used "this" points to the new object being created
 * 4. 
 *
 * write out a code example of each explanation above
@@ -12,15 +12,39 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this)
 
 // Principle 2
 
 // code example for Implicit Binding
 
+let Me ={
+    "name":"Tom"
+}
+function likes(like1,like2,like3){
+    return `Hello, my name is ${this.name} and I like: ${like1}, ${like2}, ${like3}.`;
+}
+
+console.log(likes("pizza","tacos",'beer'))
+
+
 // Principle 3
 
 // code example for New Binding
 
+function greet(greeter){
+    this.greeting ="My name is ";
+    this.greeter = greeter;
+    this.speak = function(){
+        console.log(this.greeting + this.greeter);
+    }
+};
+
+let Tommy = new greet('Tom');
+
+Tommy.speak();
+
 // Principle 4
 
 // code example for Explicit Binding
+
